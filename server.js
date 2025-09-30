f9b8a81332d5eae18c2fb301ec93dabb43e4fafa
nick.haras <nick@macexperts.gr>
2025-09-30 20:49:02 +0300

    Increase CORS permissions

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
+// Ultra-permissive CORS (allows any origin). Remove or tighten for production security.
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Handle preflight quickly
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 const APP_TOKEN = process.env['SUMSUB_APP_TOKEN'];
 const SECRET_KEY = process.env['SUMSUB_SECRET_KEY'];
 
